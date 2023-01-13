b802d69fec5073996ff1f2e1a15527e111cc5d75
emil <hoangominh01@gmail.com>
2023-01-13 08:40:39 +0700

    modified:   vue/src/environment/environment.ts

--- vue/src/environment/environment.ts
+++ vue/src/environment/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  api: "hswallowtail-music.vn/api",
-  default: "hswallowtail-music.vn/storage/default",
-  album_cover: "hswallowtail-music.vn/storage/upload/album_cover",
-  playlist_cover: "hswallowtail-music.vn/storage/upload/playlist_cover",
-  profile_image: "hswallowtail-music.vn/storage/upload/profile_image",
-  artist_image: "hswallowtail-music.vn/storage/upload/artist_image",
-  song_src: "hswallowtail-music.vn/storage/upload/song_src",
+  api: "swallowtail-music.vn/api",
+  default: "swallowtail-music.vn/storage/default",
+  album_cover: "swallowtail-music.vn/storage/upload/album_cover",
+  playlist_cover: "swallowtail-music.vn/storage/upload/playlist_cover",
+  profile_image: "swallowtail-music.vn/storage/upload/profile_image",
+  artist_image: "swallowtail-music.vn/storage/upload/artist_image",
+  song_src: "swallowtail-music.vn/storage/upload/song_src",
   // song_src:
   //   "http://localhost/laravel-learn/swallowtail-music/public/storage/upload/song_src",
 };
