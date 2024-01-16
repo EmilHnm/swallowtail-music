b35e62d3873436e28b75a64db4c7f68f88b8b028
emil <hoangominh01@gmail.com>
2024-01-16 23:37:23 +0700

    add user image storage

--- app/Helpers/file_path_helper.php
+++ app/Helpers/file_path_helper.php
@@ -2,13 +2,13 @@
 
 function file_path_helper($id)
 {
-    if ($id >= 1000000000) {
-        $parts = str_split(strrev($id), 3);
-        $parts = array_reverse($parts);
-
-        $part = implode('/', $parts);
-        return "/$part";
+    if ($id < 1000000000) {
+        $id = sprintf('%09d', $id);
     }
 
-    return null;
+    $parts = str_split(strrev($id), 3);
+    $parts = array_reverse($parts);
+
+    $part = implode('/', $parts);
+    return "/$part";
 }
