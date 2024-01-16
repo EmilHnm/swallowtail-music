b7e803b616bf3246b1b697cde6ee885c458d98b8
emil <hoangominh01@gmail.com>
2024-01-16 23:36:53 +0700

    modify user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('user_id');
+            $table->uuid('user_id');
             $table->string('email');
-            $table->string('role')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('gender')->nullable();
