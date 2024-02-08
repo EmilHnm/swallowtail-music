dcb295ef81e6ebc58bd0e3f66c5b0be358d08074
emil <hoangominh01@gmail.com>
2024-02-09 00:44:15 +0700

    Refactor requests table column names

--- database/migrations/2024_01_30_090525_create_requests_table.php
+++ database/migrations/2024_01_30_090525_create_requests_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('requests', function (Blueprint $table) {
             $table->id();
             $table->string('requester');
-            $table->string('request_type');
-            $table->string('request_status');
-            $table->json('request_description');
+            $table->string('type');
+            $table->string('status');
+            $table->json('body');
             $table->string('filled_by')->nullable();
             $table->boolean('user_fillable')->default(true);
             $table->timestamps();
