f2e284a9f3b86abdce1e9daf0d0a51b1a2510511
emil <hoangominh01@gmail.com>
2024-02-28 00:11:40 +0700

    fix eslint

--- vue/.eslintrc.cjs
+++ vue/.eslintrc.cjs
@@ -16,6 +16,6 @@ module.exports = {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
         "prettier/prettier": ["error", { endOfLine: "auto" }],
-        "scss.lint.unknownAtRules": "ignore",
+        "scss.lint.unknownAtRules": 0,
     },
 };
